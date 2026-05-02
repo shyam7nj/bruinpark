8fff6b1e426023ad65919a8b5abd650db5bf8856
Shyam Srinivasan <shyam7nj@ucla.edu>
2026-05-01 22:21:22 -0700

    feat(server): Connect to MongoDB Atlas

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 
 const express = require('express'); 
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config();
 
@@ -12,6 +13,11 @@ app.use(express.json());
 app.get(`/`, (req, res) => {
     res.send("BruinPark API running")
 })
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log("MongoDB Connected");
+});
+
 
 app.listen(port, () =>{
     console.log(`Server listening on Port ${port}`)
