1a64fea0e0dcd061022fb7a4ec778e2ddca05d3c
Shyam Srinivasan <shyam7nj@ucla.edu>
2026-05-06 21:11:32 -0700

    feat(server_user): Update User.js to have the googleId field for future implementation of Google OAuth. This field helps with looking up returning users on login.

--- server/models/User.js
+++ server/models/User.js
@@ -2,6 +2,14 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+    
+    // How the DB identifies users internally:
+    googleId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    
     email: {
         type: String,
         required: true,
@@ -12,8 +20,9 @@ const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
-    },
+    }
 
 }, { timestamps: true });
 
 module.exports = mongoose.model("User", userSchema);
+
