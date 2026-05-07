fecc21d803aa545404607637c0596b1b8899130f
Shyam Srinivasan <shyam7nj@ucla.edu>
2026-05-06 22:56:11 -0700

    feat(user_auth): Configure Passport Google OAuth strategy and update regex in User.js to support g.ucla.edu as well as ucla.edu.

--- server/models/User.js
+++ server/models/User.js
@@ -14,7 +14,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true,
-        match: [/^[A-Za-z0-9._%+-]+@ucla\.edu$/, "Please use a UCLA email."]
+        match: [/^[A-Za-z0-9._%+-]+@(g\.)?ucla\.edu$/, "Please use a UCLA email."]
     },
 
     name: {
