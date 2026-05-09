e0304a87e382a0b09908945607e765662c16e8c5
Shyam Srinivasan <shyam7nj@ucla.edu>
2026-05-08 18:16:57 -0700

    feat(auth): Mount oauth routes with '/auth'

--- server/index.js
+++ server/index.js
@@ -3,12 +3,28 @@ const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+const session = require('express-session');
+const passport = require('./config/passport');
+const authRouter = require('./routes/auth');
+
 dotenv.config();
 
 const app = express();
 const port = 3001;
 
 app.use(express.json());
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Mount: Put '/auth' in front of the routes from routes/auth.js
+app.use('/auth', authRouter);
 
 app.get(`/`, (req, res) => {
     res.send("BruinPark API running")
