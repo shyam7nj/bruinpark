462cb4bedb6b93e322b708a5d780ff05f4b5ed9a
Shyam Srinivasan <shyam7nj@ucla.edu>
2026-06-01 23:17:11 -0700

    feat(home): Redesign landing page to be more visually appealing, and use components from earlier

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,37 +4,40 @@ import AppLayout from '../components/AppLayout';
 import Button from '../components/Button';
 import Card from '../components/Card';
 import PageHeader from '../components/PageHeader';
+import '../styles/pageStyles/home.css';
 
 function Home(){
   return(
     <AppLayout>
-      <PageHeader
-        label="UCLA Commuter Parking"
-        title="Find parking partners with compatible schedules"
-        descrption="BruinPark helps UCLA commuter students connect with other each other to save money on parking permits."
-        actions={
-          <Button href={`${API_URL}/auth/google`}>
-            Log in with Google
-          </Button>
-        }
-      />
+      <section className="home-page-header">
+        <PageHeader
+          label="For UCLA Commuters"
+          title="BruinPark"
+          description="Find commuter students with compatible parking schedules, create and browse parking posts, and send message requests when you find a possible match."      
+          actions={
+            <Button href={`${API_URL}/auth/google`} variant="secondary">
+              Log in with Google
+            </Button>
+          }
+        />
+      </section>
 
       <div className="home-grid">
-        <Card>
+        <Card className="home-info-card">
           <h2>Create a post</h2>
           <p>
             Share your parking structure and weekly schedule so other commuters can find compatible matches.
           </p>
         </Card>
 
-        <Card>
+        <Card className="home-info-card">
           <h2>Browse Matches</h2>
           <p>
             Filter posts by day and/or parking structure and look for students with parking needs that work with yours.
           </p>
         </Card>
 
-        <Card>
+        <Card className="home-info-card">
           <h2>Request Contact</h2>
           <p>
             Send a message request first, then continue the conversation only after the other student accepts.
